480d810871a13d54dab769f8f00df628c375e603
An2468 <105502778+An2468@users.noreply.github.com>
2023-02-05 11:30:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-// JavaScript for updating the preview box
-function updatePreview(image) {
-  document.getElementById("preview").style.backgroundImage = "url(" + image + ")";
+var cats = document.getElementsByClassName("cat");
+var tables = document.getElementsByClassName("table");
+
+for (var i = 0; i < cats.length; i++) {
+  cats[i].addEventListener("click", function() {
+    for (var j = 0; j < cats.length; j++) {
+      cats[j].classList.remove("selected");
+    }
+    this.classList.add("selected");
+  });
+}
+
+for (var i = 0; i < tables.length; i++) {
+  tables[i].addEventListener("click", function() {
+    for (var j = 0; j < tables.length; j++) {
+      tables[j].classList.remove("selected");
+    }
+    this.classList.add("selected");
+  });
 }
